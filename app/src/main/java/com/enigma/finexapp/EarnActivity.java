d977cf54daac778f8a7f5826d01fccb3ecd7921d
Syahmi <syahmi.imran92@gmail.com>
2022-09-06 00:14:56 +0800

    New FinEd edition application update (6/9/22 - 12.14pm)
    Ui changes
    - Add iNAQ element ui
    - Add popQuestion (need backend)

--- app/src/main/java/com/enigma/finexapp/EarnActivity.java
+++ app/src/main/java/com/enigma/finexapp/EarnActivity.java
@@ -1,15 +1,19 @@
 package com.enigma.finexapp;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 public class EarnActivity extends AppCompatActivity {
 
-    CardView mEarnGrabDriver, mEarnGrabRider, mEarnOnlineBusiness, mEarnPartTime, mEarnFoodpanda, mEarnScholar, mEarnWaiter;
+    private CardView mEarnGrabDriver, mEarnGrabRider, mEarnOnlineBusiness, mEarnPartTime, mEarnFoodpanda, mEarnScholar, mEarnWaiter;
+    private Button inaqAnswer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,21 @@ public class EarnActivity extends AppCompatActivity {
         mEarnScholar = findViewById(R.id.scholarshipCard);
         mEarnWaiter = findViewById(R.id.waiterCard);
 
+        final Dialog dialog = new Dialog(EarnActivity.this);
+        //We have added a title in the custom layout. So let's disable the default title.
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //The user will be able to cancel the dialog bu clicking anywhere outside the dialog.
+        dialog.setCancelable(true);
+        //Mention the name of the layout of your custom dialog.
+        dialog.setContentView(R.layout.inaq_element_layout);
+
+        dialog.show();
+
+        inaqAnswer = dialog.findViewById(R.id.inaq_answerButton);
+
+        inaqAnswer.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
 
         mEarnGrabDriver.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/enigma/finexapp/ProtectActivity.java
+++ app/src/main/java/com/enigma/finexapp/ProtectActivity.java
@@ -2,13 +2,42 @@ package com.enigma.finexapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProtectActivity extends AppCompatActivity {
+
+    private Button askHint;
+    private ImageView checkAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protect);
+
+        final Dialog dialog = new Dialog(ProtectActivity.this);
+        //We have added a title in the custom layout. So let's disable the default title.
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //The user will be able to cancel the dialog bu clicking anywhere outside the dialog.
+        dialog.setCancelable(false);
+        //Mention the name of the layout of your custom dialog.
+        dialog.setContentView(R.layout.pop_question_layout);
+
+        dialog.show();
+
+        askHint = dialog.findViewById(R.id.popQuiz_hintButton);
+        checkAnswer = dialog.findViewById(R.id.popQuiz_checkAnswer);
+
+        askHint.setOnClickListener(v -> {
+            //Code for get Hint from the lecture
+        });
+
+        checkAnswer.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
     }
 }
